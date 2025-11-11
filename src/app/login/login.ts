c2d7d1ef38cc2407b83c476827b2fe055cec47eb
Suprit Vaidya <suprit.vaidya@gmail.com>
2025-11-11 03:50:25 +0200

    Login page changes

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  email = '';
+  password = '';
+  rememberMe = false;
 
+  onSubmit() {
+    // For demo: just alert the form values
+    alert(`Email: ${this.email}\nPassword: ${this.password}\nRemember Me: ${this.rememberMe}`);
+  }
 }
