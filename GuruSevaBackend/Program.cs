a9c03c1715f1b6092584e0720e12fb796a964e99
Suprit Vaidya <suprit.vaidya@gmail.com>
2025-11-24 06:32:21 +0200

    chhanges for environmets

--- GuruSevaBackend/Program.cs
+++ GuruSevaBackend/Program.cs
@@ -1,6 +1,17 @@
+// Add this at the top
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using GuruSevaBackend.Models;
 
+// Load .env file
+if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+{
+  Env.Load(".env.production");
+}
+else
+{
+  Env.Load();
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +31,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Build connection string from environment variables
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var dbPass = Environment.GetEnvironmentVariable("DB_PASS");
+var connectionString = $"Server=db33277.databaseasp.net; Database=db33277; User Id={dbUser}; Password={dbPass}; Encrypt=False; MultipleActiveResultSets=True";
 
 builder.Services.AddDbContext<GuruSevaDbContext>(options =>
     options.UseSqlServer(connectionString)
