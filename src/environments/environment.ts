a9b5db039bd158779f17635f85365e5d44da32f0
Suprit Vaidya <suprit.vaidya@gmail.com>
2025-11-27 00:35:33 +0200

    Updated environment links

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7037/api'
+  //apiUrl: 'https://localhost:7037/api'
+  apiUrl: 'http://krupasindhu.premiumasp.net/api/'
 };
