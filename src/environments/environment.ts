4c851fbc063b9d5734235a1dc0cf392c63aec3f7
Suprit Vaidya <suprit.vaidya@gmail.com>
2025-11-28 00:33:37 +0200

    updated environment and index for krupasindhu

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://localhost:7037/api'
-  apiUrl: 'http://krupasindhu.premiumasp.net/api'
+  apiUrl: 'https://krupasindhu.premiumasp.net/api'
 };
